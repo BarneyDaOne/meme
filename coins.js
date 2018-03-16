1efcce03a8b33451d3ed52e740a5116c5366472a
BarneyDaOne <31426257+BarneyDaOne@users.noreply.github.com>
2018-03-16 18:01:45 -0400

    Create coins.js

---
+++ coins.js
@@ -0,0 +1,11 @@
+  let coinAmt = Math.floor(Math.random() * 15) + 1;
+  let baseAmt = Math.floor(Math.random() * 15) + 1;
+  console.log(`${coinAmt} ; ${baseAmt}`);
+
+  if(coinAmt === baseAmt){
+    coins[message.author.id] = {
+      coins: coins[message.author.id].coins + coinAmt
+    };
+  fs.writeFile("./coins.json", JSON.stringify(coins), (err) => {
+    if (err) console.log(err)
+  });
