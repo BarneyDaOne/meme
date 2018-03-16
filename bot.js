f87d4e54034b45078e044ad955a9c2e818827616
BarneyDaOne <31426257+BarneyDaOne@users.noreply.github.com>
2018-03-15 20:20:51 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ let purple = 0x291F9F
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-  client.user.setPresence({ game: { name: client.users.size + 'users. (z!help)', type: 2 } });
+  client.user.setPresence({ game: { name: client.users.size + ' users. (z!help)', type: 1 } });
 });
 
 let prefix = "z!"
