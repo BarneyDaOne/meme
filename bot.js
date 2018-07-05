5ab42a8d45eba8008a526763fbee9538394349b8
BarneyDaOne <31426257+BarneyDaOne@users.noreply.github.com>
2018-07-04 21:59:25 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -566,6 +566,7 @@ const client = new Discord.Client();
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
   client.user.setPresence({ game: { name: client.users.size + ' users. (d.help)', type: 3 } });
+  msg.guild.members.get(bot.user.id).setNickname("ロボ | Deppresso Expresso")
 });
 
 let prefix = "d."
