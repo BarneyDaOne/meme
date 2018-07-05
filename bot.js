935ee8c4f6896f3ab6dfbe4cd02b677961f557d3
BarneyDaOne <31426257+BarneyDaOne@users.noreply.github.com>
2018-07-04 22:17:41 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -564,7 +564,7 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 
 client.on('ready', () => {
-  client.user.setUsername("ロボ | Deppresso"); 
+//  client.user.setUsername("ロボ | Deppresso"); 
   console.log(`Logged in as ${client.user.tag}!`);
   client.user.setPresence({ game: { name: client.users.size + ' users. (d.help)', type: 3 } });
 });
@@ -577,10 +577,9 @@ client.on('message', msg => {
 if (msg.content === prefix + "help") {
   const embed = new Discord.RichEmbed()
   .setColor(0xa7ffab)
-  .setTitle("Deppresso's Special Guide")
-  .setDescription("I honestly don't think that the guide is special...")
-  .addField("d.help", "Displays old man Deppresso's handy guide.")
-
+  .setDescription("Woah! You somehow managed to make old man Deppresso give you a help manual!")
+  .addField("Info Commands", "• d.help - Displays old man Deppresso's handy guide.")
+ 
   msg.channel.send({embed});
 };
 
