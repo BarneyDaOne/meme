594a1e3e4b2974cfc38e6c5c4b367f9423cfa26c
BarneyDaOne <31426257+BarneyDaOne@users.noreply.github.com>
2018-07-19 22:00:30 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -218,7 +218,6 @@ if (msg.content === prefix + "announce") {
   msg.channel.send({embed});
 }
 
-let warned = msg.mentions.members.first().user.username;
 let args = msg.content.split(" ").slice(1);
 let args2 = msg.content.split(" ").slice(2);
 
@@ -238,7 +237,7 @@ if (msg.content.startsWith(prefix + "warn")) {
   const embed = new Discord.RichEmbed()
   .setColor(0xff4d4f)
   .setTitle("⚠ **User Warned** ⚠")
-  .setDescription("User : " + msg.mentions.members.first().user.username + " \nWarn Giver : " + msg.author.username + " \nReason : " + args2.join(" "))
+  .setDescription("User : " + msg.mentions.members.first().username + " \nWarn Giver : " + msg.author.username + " \nReason : " + args2.join(" "))
   msg.channel.send({embed});
 }
 
