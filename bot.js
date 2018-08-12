7f41d024075a83ef3a8d23e906d3b72c79f5b31d
BarneyDaOne <31426257+BarneyDaOne@users.noreply.github.com>
2018-08-12 17:28:29 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -371,16 +371,16 @@ if (msg.content.startsWith(prefix + "buy")) {
     }
   } else if (msg.content === prefix + "buy 2") {
     if (xp[item.author.id].xp > 150) {
-      xp[item.author.id].employment = 1;
-      xp[item.author.id].job = 2;
+      xp[item.author.id].employment = 2;
+      xp[item.author.id].job = 1;
       msg.reply(xp[item.author.id].employment + xp[item.author.id].job)
     } else if (!xp[item.author.id].xp > 100) {
       msg.reply("You need at least £150 to get this job.")
     }
   } else if (msg.content === prefix + "buy 3") {
     if (xp[item.author.id].xp > 150) {
-      xp[item.author.id].employment = 1;
-      xp[item.author.id].job = 3;
+      xp[item.author.id].employment = 3;
+      xp[item.author.id].job = 1;
       msg.reply(xp[item.author.id].employment + "Emp val. / " + xp[item.author.id].job + "Jb val.")
     } else if (!xp[item.author.id].xp > 100) {
       msg.reply("You need at least £150 to get this job.")
@@ -403,16 +403,16 @@ if (msg.content.startsWith(prefix + "bal")) {
       .setDescription("• Current balance : £" + xp[item.author.id].xp + "\n• Current job : Fast food worker\n• Job type : Food")
       msg.channel.send({embed})
     }
-  } else if (xp[item.author.id].employment === 1) {
-    if (xp[item.author.id].job === 2) {
+  } else if (xp[item.author.id].employment === 2) {
+    if (xp[item.author.id].job === 1) {
       const embed = new Discord.RichEmbed()
       .setColor(0xbcffc3)
       .setTitle(msg.author.username + "'s account")
       .setDescription("• Current balance : £" + xp[item.author.id].xp + "\n• Current job : Supermarket worker\n• Job type : Service")
       msg.channel.send({embed})
     }
-  } else if (xp[item.author.id].employment === 1) {
-    if (xp[item.author.id].job === 3) {
+  } else if (xp[item.author.id].employment === 3) {
+    if (xp[item.author.id].job === 1) {
       const embed = new Discord.RichEmbed()
       .setColor(0xbcffc3)
       .setTitle(msg.author.username + "'s account")
