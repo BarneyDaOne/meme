194d794e7359876c518a54fd8e849cd38ee2a2fb
Topapo <31426257+Topapo@users.noreply.github.com>
2019-03-31 13:38:23 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,18 +2,11 @@
 
 const Discord = require("discord.js");
 const client = new Discord.Client();
-const fs = require("fs");
-let xp = require("./xp.json");
-let oxp = require("./oxp.json");
-let purple = 0x5b5b5b
-let purple2 = 0xdbdbdb
-let purple3 = 0xa580cc
-let purple4 = 0x54ff76
-const talkedRecently = new Set();
 
 client.on('ready', () => { 
   console.log(`Logged in as ${client.user.tag}!`);
   client.user.setPresence({ game: { name: client.users.size + " users. (==help)", type: 3 } });
+  client.user.setUsername("Yoit");
 });
 
 let prefix = "==";
